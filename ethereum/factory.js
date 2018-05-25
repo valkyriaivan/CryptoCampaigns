d1f194be59d452f104a1b34032c5af47552611c1
pc02 <valkyriaivan@yahoo.es>
2018-05-25 16:41:21 +0200

    Second commit

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  process.env.CONTRACT_ADDRESS
+  'https://rinkeby.infura.io/tfQAVgnTVnzV4xe6g1ao'
 );
 
 export default instance;
--- ethereum/web3.js
+++ ethereum/web3.js
@@ -7,7 +7,9 @@ if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
   web3 = new Web3(window.web3.currentProvider);
 } else {
   // We are on the server *OR* the user is not running metamask
-  const provider = new Web3.providers.HttpProvider(process.env.PROVIDER);
+  const provider = new Web3.providers.HttpProvider(
+    '0x44e10122A1e0783Fe5C8fD7F6B06fc955f33cAA5'
+  );
   web3 = new Web3(provider);
 }
 
