b803599a7d13fc4a76edef521e2dc80a03f7846b
pc02 <valkyriaivan@yahoo.es>
2018-06-01 08:50:08 +0200

    Updating enviroment variables

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -7,9 +7,7 @@ if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
   web3 = new Web3(window.web3.currentProvider);
 } else {
   // We are on the server *OR* the user is not running metamask
-  const provider = new Web3.providers.HttpProvider(
-    process.env.CONTRACT_ADDRESS
-  );
+  const provider = new Web3.providers.HttpProvider(process.env.PROVIDER);
   web3 = new Web3(provider);
 }
 
